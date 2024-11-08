c32ccd2d6e377d38c7e9129998a59968f979c179
Maximilian Faessler <maximilian@faesslerweb.de>
2024-11-08 15:36:01 +0100

    [EDIT] finalized Backend styles and GoogleGraphs

--- Resources/Public/JavaScript/Backend/Backend.js
+++ Resources/Public/JavaScript/Backend/Backend.js
@@ -1,30 +1,52 @@
 
 /* TAB JS */
+
+/*
+IMPORTANT: DO NOT WORK INITIAL WITH "display: none", BECAUSE GOOGLE GRAPH DOES NOT WORK WELL WITH THIS PROPERTY
+ */
+
+
 function openTab(evt, tabName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-  var tabContentName = 'content-' + tabName;
+      // Declare all variables
+      var i, tabcontent, tablinks;
+      var tabContentName = 'content-' + tabName;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+      // Get all elements with class="tabcontent" and hide them
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+            //tabcontent[i].style.display = "none";
+            tabcontent[i].style.visibility = "hidden";
+            tabcontent[i].style.height = 0;
+            tabcontent[i].style.border = 'none';
+      }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+      // Get all elements with class="tablinks" and remove the class "active"
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabContentName).style.display = "block";
-  evt.currentTarget.className += " active";
+      // Show the current tab, and add an "active" class to the button that opened the tab
+      //document.getElementById(tabContentName).style.display = "block";
+      document.getElementById(tabContentName).style.visibility = "visible";
+      document.getElementById(tabContentName).style.height = '100%';
+      document.getElementById(tabContentName).style.border = '1px solid #ccc';
+      evt.currentTarget.className += " active";
 }
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    /* initial: hide TABS */
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        if (i > 0) {
+            // tabcontent[i].style.display = "none";
+            tabcontent[i].style.visibility = "hidden";
+        }
+
+    }
 
     /* ACCORDION JS */
     var acc = document.getElementsByClassName("accordion");
@@ -42,6 +64,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
           } else {
               document.getElementById(targetPanel).style.display = "block";
           }
+        });
+    }
+
+    // Toggle accordions
+    var acc = document.getElementsByClassName("toggle-accordions");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            // ID of toggle button is the same as the class name of the singel panels
+            var toggleButtonId = this.getAttribute('id');
+
+            var toggleButtonIsActive = this.classList.contains('active');
+
+            // toggle "active" for accordion-header
+            document.querySelectorAll('.accordion-' + toggleButtonId).forEach(function(el) {
+                if (toggleButtonIsActive) {
+                    el.classList.remove('active');
+                } else {
+                    el.classList.add('active');
+                }
+            });
+
+            // toggle accordion-panels
+            document.querySelectorAll('.' + toggleButtonId).forEach(function(el) {
+                if (toggleButtonIsActive) {
+                    el.style.display = 'none';
+                } else {
+                    el.style.display = 'block';
+                }
+            });
+
         });
     }
 
